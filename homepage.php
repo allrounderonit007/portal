68d5fbdf1ee01b878479269ce9399acc886c70fb
Vedant Tikku <allrounderonit007@gmail.com>
2015-05-17 17:49:07 +0530

    new content

--- homepage.php
+++ homepage.php
@@ -15,7 +15,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/business-casual.css" rel="stylesheet">
+    <link href="css/homepage.css" rel="stylesheet">
 
     <!-- Fonts -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
@@ -32,12 +32,14 @@
 
 <body>
 
-    
+    <div class="brand" style="color: black; font-family: Josefin Slab ">Phd Portal</div>
+    <div class="address-bar" style="color: black; font-family: Josephin Slab">Dhirubhai Ambani Institute of Information and Communication Technology</div>
     
     
     <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-default" role="navigation">
         <div class="container-fluid">
+            
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -51,6 +53,7 @@
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                
                 <ul class="nav navbar-nav">
                     <li>
                         <a href="homepage.php">Home</a>
@@ -71,6 +74,7 @@
                         <a href="Schedule.php">Schedule</a>
                     </li>
                 </ul>
+                    
             </div>
             <!-- /.navbar-collapse -->
             
@@ -80,17 +84,16 @@
     </nav>
     
     
-    <div class="container">
+    <!--<div class="container">
         <div class="row">
             <div class="box">
                 <div style="height: 40px; width: 100%">
                     
                 </div>
-                <div class="brand">Business Casual</div>
-                <div class="address-bar">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
+                
             </div>
         </div>
-    </div>
+    </div>-->
     
     
     <div class="container">
@@ -99,16 +102,10 @@
             <div class="box">
                 <div class="col-lg-12 text-center">
                     
-                    <h2 class="brand-before">
-                        <small>Welcome to</small>
-                    </h2>
-                    <h1 class="brand-name">Business Casual</h1>
+                    
+                    <h1 class="brand-name">Academic Honesty</h1>
                     <hr class="tagline-divider">
-                    <h2>
-                        <small>By
-                            <strong>Start Bootstrap</strong>
-                        </small>
-                    </h2>
+                    
                 </div>
             </div>
         </div>
@@ -117,15 +114,143 @@
             <div class="box">
                 <div class="col-lg-12">
                     <hr>
-                    <h2 class="intro-text text-center">Build a website
-                        <strong>worth visiting</strong>
+                    <h2 class="intro-text text-center" style="font-family:Cambria">Institute Guideline on
+                        <strong>Plagiarism</strong>
                     </h2>
                     <hr>
-                    <img class="img-responsive img-border img-left" src="img/intro-pic.jpg" alt="">
-                    <hr class="visible-xs">
-                    <p>The boxes used in this template are nested inbetween a normal Bootstrap row and the start of your column layout. The boxes will be full-width boxes, so if you want to make them smaller then you will need to customize.</p>
-                    <p>A huge thanks to <a href="http://join.deathtothestockphoto.com/" target="_blank">Death to the Stock Photo</a> for allowing us to use the beautiful photos that make this template really come to life. When using this template, make sure your photos are decent. Also make sure that the file size on your photos is kept to a minumum to keep load times to a minimum.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                    
+                    <h3>Definition</h3>
+                    <p>Plagiarism includes, but is not limited to, failure to indicate the source with quotation marks or footnotes where appropriate if any of the following are reproduced in the work submitted by a student:</p>
+                    <ul><li>
+                        A phrase, written or musical.
+                        </li>
+                        <li>
+                            A graphic content.
+                        </li>
+                        <li>
+                            A proof.
+                        </li>
+                        <li>
+                            Specific Language.
+                        </li>
+                        <li>
+                            An idea derived from the work, published or unpublished, of another person.
+                        </li>
+                    </ul>
+                    
+                    <p>There are several basic factors to consider when evaluating a case of possible plagiarism:</p>
+                    <ul>
+                        
+                        <li>
+                            Amount or quantity (full paper, a section of a paper, a page, a paragraph, a sentence, phrases)
+                        </li>
+                        <li>
+                            Use of quotation marks for all copied text
+                        </li>
+                        
+                        <li>
+                            Appropriate placement of credit notices
+                        </li>
+                        
+                        <li>
+                            Improper paraphrasing
+                        </li>
+                    </ul>
+                    <h3>
+                        Possible plagiarism scenarios
+                    </h3>
+                    <p>Potentially complicating the effort to identify plagiarism is the fact that each of the above basic factors can be combined with other factors, creating a range of possible plagiarism scenarios. Here, then, is a full list of possible scenarios, starting with the worst case</p>
+                    <ul>
+                        <li>Uncredited Verbatim Copying of a Full Paper, or Uncredited Verbatim Copying of a Major Portion (more than 50%) within a Single Paper - An instance is where a large section of the original paper is copied without quotation marks, credit notice, reference, and bibliography. This case also includes instances where different portions of a paper are copied without attribution from a number of papers by other authors, and the sum of plagiarized material is more 50%,<br> 
+                            or<br>
+Uncredited Verbatim Copying within More than a Single Paper by the Same Author(s)--This includes instances where more than one paper by the offending author(s) has been found to contain plagiarized content, and all the percentages of plagiarized material in each of the discovered papers sum to greater than 50%.</li>
+                        
+                        <li>
+                            Uncredited Verbatim Copying of a Large Portion (greater than 20% and up to 50%) within a Paper.--An instance is where a section of the original paper is copied from another paper without quotation marks, credit notice, reference, and bibliography. This case also includes instances where different portions of a paper are copied without attribution from a number of papers by other authors, and the sum of copying results in a large portion of plagiarized material (up to 50%) in the paper<br>or<br>Uncredited Verbatim Copying within More than One Paper by the Same Author(s)--This includes instances where the sum of plagiarized material from the different papers would constitute the equivalent of a large portion (greater than 20% and up to 50%) of the discovered paper with the fewest words.
+                        </li>
+                        
+                        <li>
+                            Uncredited Verbatim Copying of Individual Elements (Paragraph(s), Sentence(s), Illustration(s), etc.) Resulting in a Significant Portion (up to 20%) within a Paper--An instance could be where portions of original paper are used in another paper without quotation marks, credit notice, reference, and bibliography.
+                        </li>
+                        
+                        <li>
+                            Uncredited Improper Paraphrasing of Pages or Paragraphs. Instances of improper paraphrasing occur when only a few words and phrases have been changed or when the original sentence order has been rearranged; no credit notice or reference appears with the text
+                        </li>
+                        
+                        <li>
+                            Credited Verbatim Copying of a Major Portion of a Paper without Clear Delineation. Instances could include sections of an original paper copied from another paper; credit notice is used but absence of quotation marks or offset text does not clearly reference or identify the specific, copied material
+                        </li>
+                    </ul>
+                    
+                    <h3>
+                        Plagiarism, in short
+                    </h3>
+                    
+                    <ul>
+                        <li>
+                            The extreme and more obvious cases notwithstanding, the above scenarios provide us with some basic determining factors we can use when attempting to deal with allegations of plagiarism between authors
+                        </li>
+                        
+                        <li>
+                            Amount or quantity does not play a part in defining plagiarism. However, the amount of material plagiarized should play an important part in determining the appropriate corrective action.
+                        </li>
+                        
+                        <li>
+                            Credit notices or references are not sufficient to deflecting a charge of plagiarism if quotation marks or offset text have not been used to identify the specific material being copied
+                        </li>
+                        
+                        <li>
+                            Paraphrasing can leave an author open to a charge of plagiarism if he or she has changed only a few words or phrases or has only rearranged the original sentence order. Even a proper paraphrasing of the original text can lead to a charge of plagiarism if the original source is not properly cited
+                        </li>
+                    </ul>
+                    
+                    <h3>
+                        Five Types of Plagiarism
+                    </h3>
+                    <br>
+                    <h4 style="text-transform:lowercase">Type-1 "Copy & Paste Plagiarism" </h4>
+                    <ol>
+                        <li>
+                             Any time you lift a sentence or significant phrase intact from a source, you must use quotations marks with proper reference to the source at the end of the document or in the footnote of the page. 
+                        </li>
+                        <li>
+                            You must avoid copying full sentence or more.
+                        </li>
+                        
+                    </ol>
+                    
+                    <h4 style="text-transform:lowercase">Type-2 "Word Switch Plagiarism"</h4>
+                    <ol>
+                        <li>
+                            If you take a sentence from a source and change around a few words, it is still plagiarism. If you want to quote a sentence, then you need to put it in quotation marks and cite the author and article. But quoting Source articles should only be done if what the quote says is particularly useful in the point you are trying to make in what you are writing". The best practice is to paraphrase and then cite the original sources
+                        </li>
+                        
+                    </ol>
+                    
+                    <h4 style="text-transform:lowercase">
+                        Type-3 "Style Plagiarism" 
+                    </h4>
+                    <ol>
+                        <li>
+                            "When you follow a Source Article sentence-by-sentence or paragraph-by-paragraph, it is plagiarism, even though none of your sentences are exactly like those in the source article or even in the same order. What you are copying in this case is the author's reasoning style."
+                        </li>
+                        
+                    </ol>
+                    
+                    <h4 style="text-transform:lowercase">
+                        Type-4 "Metaphor Plagiarism" 
+                    </h4>
+                    <ol>
+                    <li>"Metaphors are used either to make an idea clearer or give the reader an analogy that touches the senses or emotions better than a plain description of the object or process. Metaphors, then, are an important part of an author's creative style. If you cannot come up with your own metaphor to illustrate an important idea, then use the metaphor in the Source Article, but give the author credit for it.</li>
+                    </ol>
+                    <h4 style="text-transform:lowercase">
+                        Type-5 "Idea Plagiarism" 
+                    </h4>
+                    <ol>
+                        <li>
+                        "If the author of the source article expresses a creative idea or suggests a solution to a problem, the idea or solution must be clearly attributed to the author. Students seem to have a hard time distinguishing author's ideas and/or solutions from public domain information. Public domain information is any idea or solution about which people in the field accept as general knowledge. For example, what a black hole is and how it is defined is general knowledge. You do not need to reference a general description of a black hole. The escape velocity of earth is also general knowledge and needs no reference. The distance to the center of the Galaxy is also general knowledge. However, a new idea about how to look for black holes or a new solution to a physics problem needs to be attributed to the authors. If you don't know what is accepted as public domain in a particular field, ASK."
+                        </li>
+                    </ol>
                 </div>
             </div>
         </div>
